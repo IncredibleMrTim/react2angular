b947f9d124342d3138d7883534c5b40fdfa5a94f
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2022-09-16 10:33:39 +0100

    updated react version

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import fromPairs = require('lodash.frompairs')
 import NgComponent from 'ngcomponent'
 import * as React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
+import { createRoot, Root } from 'react-dom/client'
 
 /**
  * Wraps a React component in Angular. Returns a new Angular component.
@@ -20,37 +21,50 @@ export function react2angular<Props>(
   bindingNames: (keyof Props)[] | null = null,
   injectNames: string[] = []
 ): IComponentOptions {
-  const names = bindingNames
-    || (Class.propTypes && Object.keys(Class.propTypes) as (keyof Props)[])
-    || []
+  let root: Root
+  const names =
+    bindingNames ||
+    (Class.propTypes && (Object.keys(Class.propTypes) as (keyof Props)[])) ||
+    []
 
   return {
     bindings: fromPairs(names.map(_ => [_, '<'])),
-    controller: ['$element', ...injectNames, class extends NgComponent<Props> {
-      static get $$ngIsClass() {
-        return true
-      }
-      isDestroyed = false
-      injectedProps: { [name: string]: any }
-      constructor(private $element: IAugmentedJQuery, ...injectedProps: any[]) {
-        super()
-        this.injectedProps = {}
-        injectNames.forEach((name, i) => {
-          this.injectedProps[name] = injectedProps[i]
-        })
-      }
-      render() {
-        if (!this.isDestroyed) {
-          render(
-            <Class {...this.props} {...this.injectedProps as any} />,
-            this.$element[0]
-          )
+    controller: [
+      '$element',
+      ...injectNames,
+      class extends NgComponent<Props> {
+        static get $$ngIsClass() {
+          return true
+        }
+        isDestroyed = false
+        injectedProps: { [name: string]: any }
+        constructor(
+          private $element: IAugmentedJQuery,
+          ...injectedProps: any[]
+        ) {
+          super()
+          this.injectedProps = {}
+          injectNames.forEach((name, i) => {
+            this.injectedProps[name] = injectedProps[i]
+          })
+        }
+
+        render() {
+          if (!this.isDestroyed) {
+            root = createRoot(this.$element[0])
+            root.render(
+              <Class {...this.props} {...(this.injectedProps as any)} />
+            )
+          }
+        }
+        componentWillUnmount() {
+          this.isDestroyed = true
+          if (root) {
+            root.unmount()
+          }
+          // unmountComponentAtNode(this.$element[0])
         }
       }
-      componentWillUnmount() {
-        this.isDestroyed = true
-        unmountComponentAtNode(this.$element[0])
-      }
-    }]
+    ]
   }
 }
